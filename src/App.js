34b53da06968c3e74f380d5a5a7b25f93301abe1
sjana98 <sjana8901@gmail.com>
2023-12-07 21:51:59 +0530

    sign up api integrate

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import SignUp from './components/SignUp';
 
+
 function App() {
   return (
     <div className="App">
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,22 +1,37 @@
-import React,{useState} from 'react';
-
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function SignUp() {
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleClick = () => {
-    console.log(name, email, password);
+  const navigate = useNavigate();
+  const uri = "http://localhost:5000/register";
+
+  const collectData = async () => {
+    const createRequest = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password }),
+    };
+    const result = await fetch(uri, createRequest);
+    if (result) {
+      navigate("/");
+    };
   };
+
   return (
     <>
       <div className="signContainer">
         <h2>Sign Up Now</h2>
-          <input className='inputField' type="text" placeholder='Enter Your Name' value={name} onChange={(e)=>setName(e.target.value)} />
-          <input className='inputField' type="text" placeholder='Enter Your Email' value={email}  onChange={(e)=>setEmail(e.target.value)}/>
-          <input className='inputField' type="password" placeholder='Enter Password' value={password}  onChange={(e)=>setPassword(e.target.value)} />
-        <button type='button'onClick={handleClick}>Sign Up</button>
+        <input className='inputField' type="text" placeholder='Enter Your Name' value={name} onChange={(e) => setName(e.target.value)} />
+        <input className='inputField' type="text" placeholder='Enter Your Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input className='inputField' type="password" placeholder='Enter Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button type='button' onClick={collectData}>Sign Up</button>
       </div>
     </>
   )
