f55d0835022fc467f2e0ef848c5d12ac372565c0
sjana98 <sjana8901@gmail.com>
2023-12-18 12:33:27 +0530

    single product api integrate

--- src/components/UpdateProducts.jsx
+++ src/components/UpdateProducts.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
 
 function UpdateProducts() {
 
@@ -6,12 +7,29 @@ function UpdateProducts() {
     const [price, setPrice] = useState("");
     const [brand, setBrand] = useState("");
     const [category, setCategory] = useState("");
+
     const [errorMsg, setErrorMsg] = useState(false);
     const [confirmMsg, setConfirmMsg] = useState(false);
 
+
+    const params = useParams()
+
     const userName = JSON.parse(localStorage.getItem("user")).name;
-    
-    const handleUpdate = async () => {     
+
+    useEffect(() => {
+        getProductDetail()
+    });
+    const getProductDetail = async () => {
+        const api = `http://localhost:5000/products/${params.id}`;
+        const fetchProduct = await fetch(api);
+        const result = await fetchProduct.json();
+        setName(result.name);
+        setPrice(result.price);
+        setBrand(result.brand);
+        setCategory(result.category);
+    };
+
+    const handleUpdate = async () => {
         if (!name || !price || !brand || !category) {       // Simple form validation part
             setErrorMsg(true);
             return false;
@@ -19,6 +37,7 @@ function UpdateProducts() {
             setConfirmMsg(true);
         };
     };
+
 
     return (
         <>
@@ -30,15 +49,14 @@ function UpdateProducts() {
                 <input type="Number" className='inputField' placeholder='Enter price of product in rupee' value={price} onChange={(e) => setPrice(e.target.value)} />
                 {errorMsg && !price && <span className='validationText'>Enter updated price of product!!</span>}
 
-                <input type="text" className='inputField' placeholder='Enter brand name of product' value={brand} onChange={(e)=> setBrand(e.target.value)}/>
+                <input type="text" className='inputField' placeholder='Enter brand name of product' value={brand} onChange={(e) => setBrand(e.target.value)} />
                 {errorMsg && !brand && <span className='validationText'>Enter brand of product!!</span>}
 
-                <input type="text" className='inputField' placeholder='Enter category of product' value={category} onChange={(e)=> setCategory(e.target.value)}/>
+                <input type="text" className='inputField' placeholder='Enter category of product' value={category} onChange={(e) => setCategory(e.target.value)} />
                 {errorMsg && !category && <span className='validationText'>Enter category of product!!</span>}
 
                 <button type='submit' className='addBtn' onClick={handleUpdate}>Update</button>
                 {confirmMsg && <span className='conformationText'> {userName}, successfully update your product.</span>}
-
             </form>
         </>
     )
