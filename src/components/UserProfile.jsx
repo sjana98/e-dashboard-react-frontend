9d1771c616bd1842d683c691626af4fd3402fefd
sjana98 <sjana8901@gmail.com>
2023-12-20 18:08:00 +0530

    user account delete btn and delete api integrate

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function UserProfile() {
-    const auth = localStorage.getItem("user");
+
     const navigate = useNavigate();
+    const auth = localStorage.getItem("user");
+
     const handleLogout = () => {
         localStorage.clear("user");
         navigate("/login");
+    };
+    const handleDelete = async (id) => {
+        const api = `http://localhost:5000/account/${id}`; 
+        const createRequest = {
+            method: "Delete",
+        };
+        let result = await fetch(api, createRequest);
+        await result.json();
+        if (result) {
+            alert("Your account will permanently delete from database!!")
+            localStorage.clear("user");
+            navigate("/signup");
+        };
     };
 
     return (
@@ -16,6 +31,7 @@ function UserProfile() {
                 <h3 className="user-profile-card__name">{JSON.parse(auth).name}</h3>
                 <p className="user-profile-card__email">{JSON.parse(auth).email}</p>
                 <button className="user-profile-card__logout" onClick={handleLogout}>Logout</button>
+                <button className="user-profile-card__logout __delete" onClick={()=>handleDelete(JSON.parse(auth)._id)}>Account delete</button>
             </div>
         </>
     );
